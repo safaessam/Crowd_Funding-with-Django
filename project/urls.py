f941c8b85ced4d62c7b58862cfe6cc9aa3b9c4a8
Mikerosoft99 <kerosamy8000@gmail.com>
2024-02-25 00:05:42 +0200

    Added registration to setting.py and urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from registration.views import Registration, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home' ),
+    path('registration/', Registration.as_view(), name='registration' )
 ]
